c68073d74424df17781ea2f3b02df165f5cb6286
nogakatz246 <nogakatz1@gmail.com>
2025-06-29 17:19:14 +0300

    Fix notes on second layer

--- new_is_always_better.py
+++ new_is_always_better.py
@@ -1,94 +1,85 @@
-from scapy.all import conf, IFACES
+import struct
+import argparse
+from struct import unpack
 
-IFACE = "Intel(R) Wi-Fi 6 AX201 160MHz"
-IFACE_MAC = "7c:21:4a:f5:cd:e6"
-BROADCAST = "ff:ff:ff:ff:ff:ff"
+from scapy.all import conf, IFACES, get_if_hwaddr
+
+BROADCAST = b'\xff\xff\xff\xff\xff\xff'
+HEADER_FORMAT = "6s6s2s"
 
 
-def int_to_hex(num: int) -> hex:
+def is_multicast(mac_address: bytes) -> bool:
     """
-    Converts an integer to a hex representation.
-    :param num: the number to convert.
-    :return: the hex representation of the number.
+    Checks if the address returned is a multicast address.
+    :param mac_address: the mac address to check.
+    :return: True if it is a multicast address, False otherwise.
     """
-    return hex(num)
+    if int(mac_address[0]) % 2 == 1:
+        return True
+    return False
 
 
-def get_dst_mac(recv: bytes) -> str:
+def unpack_frame(recv: tuple) -> tuple:
     """
-    Gets the destination's mac address from the frame.
-    :param recv: the raw frame received.
-    :return: a string of the destination's mac address.
-    """
-    dst_mac = recv[1][:6]
-    dst_mac_str = ""
-    for byte in dst_mac:
-        dst_mac_str += str(int_to_hex(byte))
-    return ":".join(dst_mac_str.split("0x")[1:])
-
-
-def get_src_mac(recv: bytes) -> str:
-    """
-    Gets the source's mac address from the frame.
-    :param recv: the raw frame received.
-    :return: a string of the source's mac address.
-    """
-    src_mac = recv[1][6:12]
-    src_mac_str = ""
-    for byte in src_mac:
-        src_mac_str += str(int_to_hex(byte))
-    return ":".join(src_mac_str.split("0x")[1:])
-
-
-def get_type(recv: bytes) -> bytes:
-    """
-    Gets the type of the next layer protocol of the frame.
+    Unpacks the frame received.
     :param recv: the frame received.
-    :return: the type of the next layer protocol of the frame.
+    :return: a tuple of the different parts of the frame.
     """
-    next_layer_type = recv[1][12:14]
-    return next_layer_type
+    frame_size = len(recv[1])
+    return unpack(f"{HEADER_FORMAT}{frame_size - struct.calcsize("6s6s2s")}s", recv[1])
 
 
-def to_send(recv: bytes) -> bytes:
+def is_frame_for_me(dst_mac: bytes, iface_mac: str) -> bool:
     """
-    Gets the part of the frame to send to the next layer.
-    :param recv: the frame received.
-    :return: the part of the frame to send to the next layer.
+    Checks if a frame was meant for me.
+    :param dst_mac: the destination's mac address.
+    :param iface_mac: the interface's mac address.
+    :return: True if the frame was for me,  False otherwise.
     """
-    bytes_to_send = recv[1][12:]
-    return bytes_to_send
+    if not (dst_mac == bytearray.fromhex(iface_mac) or dst_mac == BROADCAST or is_multicast(dst_mac)):
+        return False
+    return True
 
 
-def process_frame() -> bytes:
+def process_frame(sock: conf.L2socket, iface: str) -> bytes:
     """
     Processes the frame - returns it if it belongs to the right interface, or drops it otherwise.
+    :param sock: the socket.
+    :param iface: the name of the interface.
     :return: the packet to send to the next layer, or b'' if the packet was sent to a different mac address.
     """
-    for_me = True
-    sock = conf.L2socket(iface=IFACE, promisc=True)
     recv = None
+    iface_mac = ''.join(get_if_hwaddr(iface).split(":"))
 
     # sniffing until a frame is received
     while recv is None or recv == (None, None, None):
         recv = sock.recv_raw()
+    dst_mac, src_mac, next_layer_type, payload = unpack_frame(recv)
 
     # getting the destination's mac address and deciding if the frame is for the right interface
-    dst_mac = get_dst_mac(recv)
     print(f"{dst_mac=}")
-    if not (dst_mac == IFACE_MAC or dst_mac == BROADCAST):
-        for_me = False
-    if for_me:
-        src_mac = get_src_mac(recv)
+    if is_frame_for_me(dst_mac, iface_mac):
         print(f"{src_mac=}")
-        next_layer_type = get_type(recv)
         print(f"{next_layer_type=}")
-        return to_send(recv)
+        return next_layer_type + payload
     return b''
 
 
+def parse_arguments() -> argparse.Namespace:
+    """
+    Parses the arguments of the command line.
+    :return: the arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('iface', type=str)
+    args = parser.parse_args()
+    return args
+
+
 def main() -> None:
-    print("for the next layer:  ", process_frame())
+    args = parse_arguments()
+    sock = conf.L2socket(iface=args.iface, promisc=True)
+    print("for the next layer:  ", process_frame(sock, args.iface))
 
 
 if __name__ == "__main__":
