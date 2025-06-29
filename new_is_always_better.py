448b0d12ecd50a4843806d248730f1ec2688c0c9
nogakatz246 <nogakatz1@gmail.com>
2025-06-29 11:00:13 +0300

    Fix notes

--- new_is_always_better.py
+++ new_is_always_better.py
@@ -1,8 +1,13 @@
-from scapy.all import conf, IFACES
+from scapy.all import conf
+import argparse
+from struct import unpack
 
-IFACE = "Intel(R) Wi-Fi 6 AX201 160MHz"
-IFACE_MAC = "7c:21:4a:f5:cd:e6"
-BROADCAST = "ff:ff:ff:ff:ff:ff"
+
+BROADCAST = b'\xff\xff\xff\xff\xff\xff'
+DST_MAC_INDEX = 0
+SRC_MAC_INDEX = 1
+TYPE_INDEX = 2
+PAYLOAD_INDEX = 3
 
 
 def int_to_hex(num: int) -> hex:
@@ -14,81 +19,77 @@ def int_to_hex(num: int) -> hex:
     return hex(num)
 
 
-def get_dst_mac(recv: bytes) -> str:
-    """
-    Gets the destination's mac address from the frame.
-    :param recv: the raw frame received.
-    :return: a string of the destination's mac address.
-    """
-    dst_mac = recv[1][:6]
-    dst_mac_str = ""
-    for byte in dst_mac:
-        dst_mac_str += str(int_to_hex(byte))
-    return ":".join(dst_mac_str.split("0x")[1:])
-
-
-def get_src_mac(recv: bytes) -> str:
-    """
-    Gets the source's mac address from the frame.
-    :param recv: the raw frame received.
-    :return: a string of the source's mac address.
-    """
-    src_mac = recv[1][6:12]
-    src_mac_str = ""
-    for byte in src_mac:
-        src_mac_str += str(int_to_hex(byte))
-    return ":".join(src_mac_str.split("0x")[1:])
-
-
-def get_type(recv: bytes) -> bytes:
-    """
-    Gets the type of the next layer protocol of the frame.
-    :param recv: the frame received.
-    :return: the type of the next layer protocol of the frame.
-    """
-    next_layer_type = recv[1][12:14]
-    return next_layer_type
-
-
-def to_send(recv: bytes) -> bytes:
+def to_send(fields: tuple) -> bytes:
     """
     Gets the part of the frame to send to the next layer.
-    :param recv: the frame received.
+    :param fields: the fields of the processed frame received.
     :return: the part of the frame to send to the next layer.
     """
-    bytes_to_send = recv[1][12:]
+    bytes_to_send = fields[TYPE_INDEX] + fields[PAYLOAD_INDEX]
     return bytes_to_send
 
 
-def process_frame() -> bytes:
+def is_multicast(recv: tuple) -> bool:
+    """
+    Checks if the address returned is a multicast address.
+    :param recv: the message received.
+    :return: True if it is a multicast address, False otherwise.
+    """
+    if int(recv[1][0]) % 2 == 1:
+        return True
+    return False
+
+
+def unpack_frame(recv: tuple) -> tuple:
+    """
+    Unpacks the frame received.
+    :param recv: the frame received.
+    :return: a tuple of the different parts of the frame.
+    """
+    frame_size = len(recv[1])
+    return unpack(f"6s6s2s{frame_size - 14}s", recv[1])
+
+
+def process_frame(iface: str, iface_mac: str) -> bytes:
     """
     Processes the frame - returns it if it belongs to the right interface, or drops it otherwise.
     :return: the packet to send to the next layer, or b'' if the packet was sent to a different mac address.
     """
     for_me = True
-    sock = conf.L2socket(iface=IFACE, promisc=True)
+    sock = conf.L2socket(iface=iface, promisc=True)
     recv = None
 
     # sniffing until a frame is received
     while recv is None or recv == (None, None, None):
         recv = sock.recv_raw()
+    fields = unpack_frame(recv)
 
     # getting the destination's mac address and deciding if the frame is for the right interface
-    dst_mac = get_dst_mac(recv)
+    dst_mac = fields[DST_MAC_INDEX]
     print(f"{dst_mac=}")
-    if not (dst_mac == IFACE_MAC or dst_mac == BROADCAST):
+    is_multicast(recv)
+    if not (dst_mac == bytearray.fromhex(iface_mac) or dst_mac == BROADCAST or is_multicast(recv)):
         for_me = False
     if for_me:
-        src_mac = get_src_mac(recv)
+        src_mac = fields[SRC_MAC_INDEX]
         print(f"{src_mac=}")
-        next_layer_type = get_type(recv)
+        next_layer_type = fields[TYPE_INDEX]
         print(f"{next_layer_type=}")
-        return to_send(recv)
+        return to_send(fields)
     return b''
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('iface', type=str)
+    parser.add_argument('iface_mac', type=str)
+    args = parser.parse_args()
+    return args
+
+
 def main() -> None:
-    print("for the next layer:  ", process_frame())
+    args = parse_arguments()
+    print("for the next layer:  ", process_frame(args.iface, args.iface_mac))
 
 
 if __name__ == "__main__":
